18d5c5f431394a53001947d71f3446a6330a481c
putdanil <putdanil@gmail.com>
2024-03-21 14:45:18 +0400

    Revert "Update pipeline_controlnet_inpaint.py"
    This reverts commit b6e1cbb7bede6c0521257355eb9a47dfc02fa4da.

--- src/diffusers/pipelines/controlnet/pipeline_controlnet_inpaint.py
+++ src/diffusers/pipelines/controlnet/pipeline_controlnet_inpaint.py
@@ -1513,12 +1513,6 @@ class StableDiffusionControlNetInpaintPipeline(
                         controlnet_cond_scale = controlnet_cond_scale[0]
                     cond_scale = controlnet_cond_scale * controlnet_keep[i]
 
-                if num_channels_unet == 9:
-                    if isinstance(controlnet_keep[i], list):
-                        control_model_input = [latent_model_input, mask, masked_image_latents]
-                    else:
-                        control_model_input = torch.cat([latent_model_input, mask, masked_image_latents], dim=1)
-
                 down_block_res_samples, mid_block_res_sample = self.controlnet(
                     control_model_input,
                     t,
