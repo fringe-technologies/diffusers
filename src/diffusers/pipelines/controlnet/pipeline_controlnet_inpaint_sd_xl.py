d23dc91247089febb242f7f981b48b3de523847a
Danil <75547456+putdanil@users.noreply.github.com>
2024-04-20 16:40:11 +0400

    Update pipeline_controlnet_inpaint_sd_xl.py

--- src/diffusers/pipelines/controlnet/pipeline_controlnet_inpaint_sd_xl.py
+++ src/diffusers/pipelines/controlnet/pipeline_controlnet_inpaint_sd_xl.py
@@ -1676,7 +1676,11 @@ class StableDiffusionXLControlNetInpaintPipeline(
                     masked_image_latents = masked_image_latents.chunk(2)[-1]
                     add_text_embeds = add_text_embeds.chunk(2, dim=0)[-1]
                     add_time_ids = add_time_ids.chunk(2, dim=0)[-1]
-                    
+
+                logger.warning(
+                    f"You have {latents.shape} ControlNets and you have passed"
+                    " prompts. The conditionings will be fixed across the prompts."
+                )
                 latent_model_input = torch.cat([latents] * 2) if do_classifier_free_guidance else latents
 
                 # concat latents, mask, masked_image_latents in the channel dimension
